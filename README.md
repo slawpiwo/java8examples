771ed1d8c39b9919d17e415e8cbac6ec243a79ae
Sławek Piwowarski <Sawek Piwowarski>
2017-02-10 17:55:32 +0100

    part1 - Using Comparator

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ part1:
  - Passing code with behavior parametrization
  - Filtering with custom predicate class and anonymous class
  - Filtering with GeneralFilter
+ - Sorting with Comparator
+ - Using Comparator
--- src/test/java/part1/GeneralFilterTest.java
+++ src/test/java/part1/GeneralFilterTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import share.Apple;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,5 +43,37 @@ public class GeneralFilterTest {
 
         //then
         assertThat(result).containsExactly(2, 4, 6, 8);
+    }
+
+    @Test
+    public void shouldSortDescendingOddNumbers() {
+        //given
+        GeneralFilter<Integer> numbersFilter = new GeneralFilter<>();
+
+        //when
+        List<Integer> result = numbersFilter.filter(numbers, n -> n % 2 == 1);
+        result.sort((n1, n2) -> n2 - n1);
+
+        //then
+        assertThat(result).containsExactly(9, 7, 5, 3, 1);
+    }
+
+    @Test
+    public void shouldSortDescendingOddNumbersWithDefinedComparator() {
+        //given
+        GeneralFilter<Integer> numbersFilter = new GeneralFilter<>();
+
+        Predicate<Integer> integerPredicate = n -> n % 2 == 1;
+        Comparator<Integer> integerComparator = (n1, n2) -> {
+            System.out.println("Compare(" + n1 + "," + n2 + ")");
+            return n2 - n1;
+        };
+
+        //when
+        List<Integer> result = numbersFilter.filter(numbers, integerPredicate);
+        result.sort(integerComparator);
+
+        //then
+        assertThat(result).containsExactly(9, 7, 5, 3, 1);
     }
 }
